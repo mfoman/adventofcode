42380f5ed33fb93d710655d8fdcbe277a016a6ad
mfoman <frederikblunck@gmail.com>
2020-12-04 18:26:25 +0100

    Not working d3p2.

--- src/bin/d3-native.rs
+++ src/bin/d3-native.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 /*
 arboreal genetics and biome stability
 
@@ -6,14 +8,16 @@ movement: 3 right, 1 down
 fn main() {
 	let content = read_file(".\\assets\\3d-input.txt");
 	let lines = vec_from_string(content);
-	let answer = solution_one(lines);
+
+	// let answer = solution_one(lines);
+	let answer = solution_two(&lines);
 
 	println!("Number of tress: {}", answer);
 }
 
-fn solution_one(lines: Vec<String>) -> i32 {
+fn solution_one(lines: &Vec<String>, walk: &(usize, usize)) -> i64 {
 	// (row, col)
-	let mut index = (0, 0);
+	let mut index: (usize, usize) = walk.clone();
 	let travel = (1, 3);
 	let mut trees = 0;
 
@@ -28,6 +32,28 @@ fn solution_one(lines: Vec<String>) -> i32 {
 	}
 
 	trees
+}
+
+fn solution_two(lines: &Vec<String>) -> i64 {
+	let walks = [
+		(1, 1),
+		(1, 3),
+		(1, 5),
+		(1, 7),
+		(2, 1),
+	];
+
+	let mut answers = vec![];
+
+	for (i, walk) in walks.iter().enumerate() {
+		let answer = solution_one(&lines, walk);
+		println!("{}: {}", i, answer);
+		answers.push(answer);
+	};
+
+	let out: i64 = answers.iter().fold(1, |acc, x| acc * x);
+
+	out
 }
 
 fn read_file(path: &str) -> String {
